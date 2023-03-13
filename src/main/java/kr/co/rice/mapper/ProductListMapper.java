c6f214d40b4b4001ee0a582075799e9a2f3ebb63
이강일 <leki305@gmail.com>
2023-03-13 11:32:11 +0900

    [Conflict] product list

--- src/main/java/kr/co/rice/mapper/ProductListMapper.java
+++ src/main/java/kr/co/rice/mapper/ProductListMapper.java
@@ -2,9 +2,14 @@ package kr.co.rice.mapper;
 
 import java.util.ArrayList;
 
+import kr.co.rice.vo.ProductImagesVo;
 import kr.co.rice.vo.ProductVo;
 
 public interface ProductListMapper {
 	public ArrayList<ProductVo> category_list(int category_id, int subcategory_id);
+	public void product_views(int product_id);
+	public ProductVo detail(int product_id);
+	public ArrayList<ProductImagesVo> getProductImage(int product_id);
+	public ArrayList<ProductVo> recommend_products();
 	public ArrayList<ProductVo> getItems(int category_id,int subcategory_id,int start, int end);
 }
